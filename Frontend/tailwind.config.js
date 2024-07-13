69a88b2df30f5e99f1754527bc7d08184a762fed
Katie Davies <millerkatie1990@gmail.com>
2024-07-13 16:13:45 +1200

    Add custom colors

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -7,6 +7,13 @@ export default {
         sueEllen: ['"Sue Ellen Francisco"', 'cursive'],
         montserrat: ['Montserrat', 'sans-serif'],
       },
+      colors: {
+        customGreen: '#588c7e',
+        burntOrange: '#F2762e',
+        lightOrange: '#F2913d',
+        dustyPink: '#BF8C80',
+        backgroundWhite: '#F2F2F2',
+      },
     },
   },
   plugins: [],
