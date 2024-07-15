02c0543912f6e762d3c99408ab0bded1126343bf
Katie Davies <millerkatie1990@gmail.com>
2024-07-15 12:29:29 +1200

    working conditionals

--- Frontend/src/components/Nav.tsx
+++ Frontend/src/components/Nav.tsx
@@ -5,12 +5,39 @@ function Nav() {
   const location = useLocation()
   return (
     <>
-      <img src={logo} alt="logo" className="w-28 h-auto" />
-      {location.pathname === '/activities' ? (
-        <Link to="favourites">My Favourites </Link>
-      ) : (
-        <Link to="activities">Activities</Link>
-      )}
+      <div className="flex justify-start flex-wrap content-center">
+        <img src={logo} alt="logo" className="w-20 h-auto" />
+        {location.pathname === '/activities' ? (
+          <Link
+            to="favourites"
+            className="font-sueEllen text-2xl text-customGreen p-3 self-center"
+          >
+            My Favourites
+          </Link>
+        ) : location.pathname === '/favourites' ? (
+          <Link
+            to="activities"
+            className="font-sueEllen text-2xl text-customGreen p-3 self-center"
+          >
+            Activities
+          </Link>
+        ) : (
+          <>
+            <Link
+              to="activities"
+              className="font-sueEllen text-2xl text-customGreen p-3 self-center"
+            >
+              Activities
+            </Link>
+            <Link
+              to="favourites"
+              className="font-sueEllen text-2xl text-customGreen p-3 self-center"
+            >
+              My Favourites
+            </Link>
+          </>
+        )}
+      </div>
     </>
   )
 }
