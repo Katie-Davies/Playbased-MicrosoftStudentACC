ab6a8317060b45627358bc8d3d6d74a5380a6dfe
Katie Davies <millerkatie1990@gmail.com>
2024-07-15 12:36:32 +1200

    fix to bottom

--- Frontend/src/pages/Layout.tsx
+++ Frontend/src/pages/Layout.tsx
@@ -12,7 +12,7 @@ function Layout() {
           <Nav />
           <Outlet />
         </main>
-        <footer className="mt-auto">
+        <footer className="mt-auto fixed bottom-0 left-0 z-20 w-full p-4">
           <p className="ml-10 text-green-800">Bright Beginnings ltd 2024</p>
         </footer>
       </div>
