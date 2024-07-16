42049d280c5f49189d7a57af3428fe653692293f
Katie Davies <millerkatie1990@gmail.com>
2024-07-16 13:25:40 +1200

    basic layout

--- Frontend/src/pages/Favourites.tsx
+++ Frontend/src/pages/Favourites.tsx
@@ -1,5 +1,33 @@
+import Button from '../components/Button'
+import SmallCard from '../components/smallCard'
+
+const data = {
+  name: 'katie',
+  email: 'millerjeor@me.com',
+}
+
 function Favourites() {
-  return <h1>Favourites</h1>
+  return (
+    <>
+      <div className="flex flex-col">
+        <h1 className="font-sueEllen text-4xl text-customGreen mb-10 text-center">
+          My Favourites
+        </h1>
+        <h1 className="font-sueEllen text-2xl text-customGreen mb-3 ml-5">
+          My details
+        </h1>
+        <p className="ml-5 mb-2">Name: {data.name}</p>
+        <p className="ml-5 mb-5">Email: {data.email}</p>
+        <Button className="text-lg w-24 ml-24 mb-10">Edit</Button>
+      </div>
+      <div>
+        <h1 className="font-sueEllen text-2xl text-customGreen mb-3 ml-5">
+          Favourites
+        </h1>
+        <SmallCard />
+      </div>
+    </>
+  )
 }
 
 export default Favourites
