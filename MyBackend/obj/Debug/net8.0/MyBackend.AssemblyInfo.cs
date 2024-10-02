48d62bb1407a70bcf160b72eab0d65cf0bea9697
Katie Davies <millerkatie1990@gmail.com>
2024-10-02 13:16:24 +1300

    working test connection

--- MyBackend/obj/Debug/net8.0/MyBackend.AssemblyInfo.cs
+++ MyBackend/obj/Debug/net8.0/MyBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7939738d0b7e174c4ece42a2aa4c38c9d8edce65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23c656d3bb440beaaa03875741f574bdce2f3728")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
